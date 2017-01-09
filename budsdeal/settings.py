71c0a3a7fa1c8f54adeda598ed1d8f94090fc91f
Peter Liu <onhout@gmail.com>
2017-01-09 10:02:09 -0800

    changed settings back to sqlite3

--- budsdeal/settings.py
+++ budsdeal/settings.py
@@ -84,13 +84,8 @@ WSGI_APPLICATION = 'budsdeal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'budsdeal',
-        'USER': 'pl',
-        'PASSWORD': 'cocacola',
-        'HOST': 'localhost',
-        'PORT': '',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
