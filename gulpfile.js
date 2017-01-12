b0f260c4ca40145a84ccebc25bca168859f45421
Peter Liu <onhout@gmail.com>
2017-01-11 16:25:37 -0800

    added concatenation of less/css files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     gautoprefixer = require('gulp-autoprefixer'),
     gless = require('gulp-less'),
+    gminifycss = require('gulp-minify-css'),
     gconcat = require('gulp-concat'),
     guglify = require('gulp-uglify');
 
@@ -25,8 +26,10 @@ gulp.task('buildcss', function(){
 
     return gulp.src('**/static/less/*.less')
         .pipe(gless(lessOptions))
+        .pipe(gconcat('styles.css'))
+        .pipe(gminifycss())
         .pipe(gautoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest('static'))
+        .pipe(gulp.dest('./static/'))
 });
 
 gulp.task('watch', function(){
