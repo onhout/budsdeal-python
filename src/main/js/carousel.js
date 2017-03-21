f66c654b421675beea8561c7c67bac1650d8abd3
Peter Liu <onhout@gmail.com>
2017-03-20 22:30:55 -0700

    removed css transition structure JS files, test run

---
+++ src/main/js/carousel.js
@@ -0,0 +1,47 @@
+class BudsCarousel {
+    // constructor(element) {
+    //     this.element = element
+    // }
+
+    static carousel(element) {
+        $(element).slick({
+            arrows: true,
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplayspeed: 5000,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: false
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false
+                    }
+                }
+            ]
+        });
+    }
+
+}
+
+export default BudsCarousel;
--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,39 +1,5 @@
 import "../less/main.less";
+var budscarousel = require('./carousel').default;
 $(function () {
-    $('#featured-carousel').slick({
-        arrows: true,
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplayspeed: 5000,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    arrows: false
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false
-                }
-            }
-        ]
-    });
+    budscarousel.carousel('#featured-carousel');
 });
